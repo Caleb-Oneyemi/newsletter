5a608898cae9b42a9cbf71e514c6a21085d35d73
Caleb Oneyemi <caloneyemi@gmail.com>
2024-04-19 19:09:54 +0100

    added config

---
+++ src/config.rs
@@ -0,0 +1,26 @@
+use config::{Config, ConfigError, File};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub struct AppSettings {
+    pub port: u16,
+    pub db: DatabaseSettings,
+}
+
+#[derive(Deserialize)]
+pub struct DatabaseSettings {
+    pub name: String,
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+}
+
+pub fn get_config() -> Result<AppSettings, ConfigError> {
+    let settings = Config::builder()
+        .add_source(File::with_name("config"))
+        .build()
+        .unwrap();
+
+    settings.try_deserialize::<AppSettings>()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
+use newsletter::config::get_config;
 use newsletter::startup::run;
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:7777").expect("failed to bind port");
+    println!("starting server ....");
+
+    let config = get_config().expect("failed to read config.yaml");
+    let addr = format!("127.0.0.1:{}", config.port);
+    let listener = TcpListener::bind(addr).expect("failed to bind port");
 
     run(listener)?.await
 }
