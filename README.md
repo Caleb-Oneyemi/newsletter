6a487bb12108bf003b4ccf20f37377742ddf97e7
Caleb Oneyemi <caloneyemi@gmail.com>
2024-04-19 18:19:03 +0100

    added db init script and migrations

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # newsletter
+
+An email newsletter system built with rust
+
+## Requirements
+
+- [Rust Toolchain](https://rustup.rs/)
+- [SQLx CLI](https://crates.io/crates/sqlx-cli): `cargo install sqlx-cli --no-default-features --features rustls,postgres`
+
+## Setup
+
+Run Postgres Container and Create Newsletter Database:
+
+```sh
+chmod +x scripts/init_db.sh
+
+scripts/init_db.sh
+```
+
+If Postgres Container is already running then run with SKIP_DOCKER flag:
+
+```sh
+chmod +x scripts/init_db.sh
+
+SKIP_DOCKER=true scripts/init_db.sh
+```
---
+++ scripts/init_db.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -x
+set -eo pipefail
+
+if ! [ -x "$(command -v sqlx)" ]; then
+    echo >&2 "Error: sqlx is not installed."
+    echo >&2 "Use:"
+    echo >&2 "    cargo install sqlx-cli --no-default-features --features rustls,postgres"
+    echo >&2 "to install it."
+    exit 1
+fi
+
+# use env variables for db with defaults if not present
+DB_USER="${POSTGRES_USER:=postgres}"
+DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+DB_NAME="${POSTGRES_DB:=newsletter}"
+DB_PORT="${POSTGRES_PORT:=5432}"
+DB_HOST="${POSTGRES_HOST:=localhost}"
+
+# Allow to skip Docker if a dockerized Postgres database is already running
+if [[ -z "${SKIP_DOCKER}" ]]
+then
+    # run postgres with docker in detach mode with maximum 1k connections
+    docker run \
+        -e POSTGRES_USER=${DB_USER} \
+        -e POSTGRES_PASSWORD=${DB_PASSWORD} \
+        -e POSTGRES_DB=${DB_NAME} \
+        -p "${DB_PORT}":5432 \
+        -d \
+        --name "newsletter_db" \
+        postgres -N 1000
+fi
+
+export PGPASSWORD="${DB_PASSWORD}"
+# Keep pinging Postgres until it's ready to accept commands
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+    >&2 echo "Postgres is still unavailable - sleeping"
+    sleep 1 
+done
+
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
+
+export DATABASE_URL=postgres://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+
+# create uses DATABASE_URL to execute
+sqlx database create
+
+sqlx migrate run
