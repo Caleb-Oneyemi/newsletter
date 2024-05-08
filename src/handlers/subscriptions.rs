16aa29dddb2cc423158453579250da7860f39b2d
Caleb Oneyemi <caloneyemi@gmail.com>
2024-05-08 20:04:52 +0100

    modified parsing and conversion to NewSubscriber to use try_into trait

--- src/handlers/subscriptions.rs
+++ src/handlers/subscriptions.rs
@@ -15,6 +15,16 @@ pub struct SubscriberFormData {
     name: String,
 }
 
+impl TryFrom<SubscriberFormData> for NewSubscriber {
+    type Error = String;
+
+    fn try_from(data: SubscriberFormData) -> Result<Self, Self::Error> {
+        let name = SubscriberName::parse(data.name.clone())?;
+        let email = SubscriberEmail::parse(data.email.clone())?;
+        Ok(NewSubscriber { email, name })
+    }
+}
+
 #[tracing::instrument(
     name = "Create Subscriber",
     skip(form, conn),
@@ -24,23 +34,14 @@ pub struct SubscriberFormData {
     )
 )]
 pub async fn subscribe(form: Form<SubscriberFormData>, conn: Data<PgPool>) -> HttpResponse {
-    let name = match SubscriberName::parse(form.name.clone()) {
-        Ok(name) => name,
+    //implementing TryForm automatically gives us the reverse: try_into.
+    //conversion could alternatively be done with NewSubscriber::try_from(form.0)
+    let new_subscriber = match form.0.try_into() {
+        Ok(sub) => sub,
         Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-
-    let email = match SubscriberEmail::parse(form.email.clone()) {
-        Ok(name) => name,
-        Err(_) => return HttpResponse::BadRequest().finish(),
-    };
-
-    let new_subscriber = NewSubscriber {
-        email,
-        name,
     };
 
     let res = create_subscriber(&conn.get_ref(), &new_subscriber).await;
-
     match res {
         Ok(_) => HttpResponse::Created().finish(),
         Err(_) => HttpResponse::Conflict().finish(),
